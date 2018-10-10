d496842b7f2b46af126db69edfd9fbb19a5601cc
Girish Verma <girishv@thoughtworks.com>
2018-10-10 15:03:33 +0530

    Install app via kubectl commands

--- app-deployment-kubectl.sh
+++ app-deployment-kubectl.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-API_SERVER="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT"
-CA_CRT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-
-kubectl --server="$API_SERVER" --certificate-authority="$CA_CRT" --token="$TOKEN" get pods
+kubectl apply -f metadata-deployment.json
+kubectl apply -f metadata-service.json
